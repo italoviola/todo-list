8987f4687a89aa266b631828133b5db8e85eda02
italoviola <italo.viola@fbiz.com.br>
2021-09-28 18:43:26 -0300

    Layout adjusts

--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -22,6 +22,7 @@ function TodoList() {
     <div className="todo-list">
       <TodoListInput onSubmit={addTasks}/>
       <div className={`todo-list-tasks ${ tasks.length > 0 ? '' : '-no-tasks'}`}>
+        {tasks.length > 0 ? '' : <div className={'no-tasks-disclaimer'}><span>Nenhum item cadastrado</span></div>}
         {tasks.map((item)=>{
           return <TodoListTask key={item.id} {...item} onClick={removeTask}/>
         })}
